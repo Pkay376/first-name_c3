15413ded1d6b7c2af5fe935d8d9ac2ebce28ba00
Pkay376 <puritykidiavai@gmail.com>
2023-08-28 22:00:36 +0300

    second commit message

---
+++ public/portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function portfolio() {
+    return (
+        <div className = "portfolio">
+            <h1>Full Name</h1>
+            <p>Purity Kidiavai</p>
+
+            <h2>Educational Background</h2>
+            <p>Cerificate in Frontend Development</p>
+
+            <h2>Professionnal Background</h2>
+            <p>Frontend Engineer at NYC</p>
+
+            <h2>Skills</h2>
+            <ul>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+            </ul>
+
+        </div>
+    );
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
+import Portfolio from './Portfolio';
 
 function App() {
   return (
-    <div>
-       <h1>Hello world</h1>
-        <p>I am [Purity Kidiavai],this is my first React application.</p>
+    <div className="App">
+      <Portfolio />
     </div>
   );
 }
 
 export default App;
+
+
+
+
