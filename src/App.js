b4d41d347fb43124b5c04cc5c8c1cd766cf60e0b
Pkay376 <puritykidiavai@gmail.com>
2023-08-28 20:55:46 +0300

    My first react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <h1>Hello world</h1>
+        <p>I am [Purity Kidiavai],this is my first React application.</p>
+    </div>
+  );
+}
+
+export default App;
